7a70449f5dd17a729573d05271cd8ae74384fbcb
Richard Price <mrspondooli@gmail.com>
2017-10-29 13:43:44 +0000

    project groups added

---
+++ Controller/TeamsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  TeamsViewController.swift
+//  WC2018Info
+//
+//  Created by Richard Price on 26/10/2017.
+//  Copyright © 2017 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class TeamsViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    //--MARK-----arrays to hold teams from API here
+//    var teams:      [Teams] = []
+//    var teasmsCopy: [Teams] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    
+            //navbar customise here
+        
+    }
+    
+    func loadTeams() {
+        //API get teams
+        
+    }
+    
+    func doSearch() {
+        
+        //searchbar filtering
+        
+    }
+    
+    func transition() {
+        
+        //transition for switching to team details VC
+        
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+
+
+
