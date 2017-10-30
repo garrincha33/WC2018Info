01adc95457388f2e280f45f7efcedd67a77339ce
Richard Price <mrspondooli@gmail.com>
2017-10-30 07:45:36 +0000

    added getTeams & observeTeam for pulling data from firebase

--- Apis/TeamsApi.swift
+++ Apis/TeamsApi.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+class TeamApi {
+    
+    var REF_TEAMS = Database.database().reference().child("teams")
+    
+    //--use in TeamsVC to load team flags preparing for swwitch to teamsDetailsVC
+    func getTeams(completion: @escaping(Teams) -> Void) {
+        REF_TEAMS.observeSingleEvent(of: .value) { (snapshot: DataSnapshot) in
+            let arraySnap = (snapshot.children.allObjects as! [DataSnapshot]).reversed()
+            arraySnap.forEach({ (child) in
+                if let dict = child.value as? [String: Any] {
+                    let team = Teams.init()
+                    let result = team.transformTeam(dict: dict, key: child.key)
+                    completion(result)
+                }
+            })
+        }
+    }
+    
+    //--use in get team detail to display team data when cell is tapped
+    func observeTeams(withId id: String, completion: @escaping (Teams) -> Void) {
+        REF_TEAMS.child(id).observe(.value) { (snapshot: DataSnapshot) in
+            if let dict = snapshot.value as? [String: Any] {
+                let team = Teams.init()
+                let result = team.transformTeam(dict: dict, key: snapshot.key)
+                completion(result)
+            }
+        }
+    }
+}
